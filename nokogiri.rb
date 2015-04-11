a2fbfe5abe16d757bdcb5780dbc90954b9105ee3
lion-san <yokoi.shinya@jp.fujitsu.com>
2015-04-11 21:01:59 +0900

    first commit

---
+++ nokogiri.rb
@@ -0,0 +1,20 @@
+# encoding: UTF-8
+require 'nokogiri'
+require 'open-uri'
+ 
+host = 'http://tv.so-net.ne.jp'
+url = host + '/chart/23.action?head=201504120000&'
+   
+html = open(url)
+doc = Nokogiri::HTML(html)
+
+puts doc.css('a').length
+
+doc.css('a').each do |item|
+  if /^\/iepg.tvpi/ =~ item[:href] then
+    puts host+item[:href]
+    iepg = open(host+item[:href])
+    tv = Nokogiri::HTML.parse(iepg, nil, "Shift_JIS")
+    puts tv.to_s.encode("UTF-8", "Shift_JIS")
+  end
+end
